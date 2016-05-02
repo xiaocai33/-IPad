bea63fe795a87dc067a1d715873f8bd5268ec2e2
xiaocai <tycai1@sina.com>
2016-05-02 14:43:41 +0800

    根据 dropdownView 的尺寸, 设置popover的尺寸

--- 美团IPad/美团IPad/Classes/Home(首页)/Controller/MTCategoryViewController.m
+++ 美团IPad/美团IPad/Classes/Home(首页)/Controller/MTCategoryViewController.m
@@ -20,10 +20,13 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    //设置popover的尺寸
-    self.preferredContentSize = CGSizeMake(320, 480);
+    
+    
     
     MTHomeDropdownView *dropdownView = [[MTHomeDropdownView alloc] init];
+    
+    //根据 dropdownView 的尺寸, 设置popover的尺寸
+    self.preferredContentSize = CGSizeMake(dropdownView.width, CGRectGetMaxY(dropdownView.frame));
     
     [self.view addSubview:dropdownView];
     
